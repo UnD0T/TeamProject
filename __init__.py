54ed4ed3e4a4bfbef09e94ffcc19f02ec2fe043a
Crais2905 <162161234+Crais2905@users.noreply.github.com>
2024-07-23 19:13:00 +0300

    Second test

--- __init__.py
+++ __init__.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
+
 
 from app import routes, models
