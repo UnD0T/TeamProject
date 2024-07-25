e443191800ab5984ee53953afd586ca972489553
Crais2905 <slisovets2905@gmail.com>
2024-07-25 14:29:58 +0300

    edit config

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '123456789'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(BASEDIR, 'auth.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(BASEDIR, 'teamproject.db')
