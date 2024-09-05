e5a738f71e27ae7b313568fac25881c44b815015
Crais2905 <slisovets2905@gmail.com>
2024-09-05 21:46:04 +0300

    fix style link in index

--- app/static/js/product_detail.js
+++ app/static/js/product_detail.js
@@ -10,12 +10,20 @@
 //     }
 
 
-document.querySelector("product-image").onclick = () => {
-    document.querySelector('.pop-up').style.display = 'block'
-    document.querySelector('.pop-up img').src = document.querySelector("product-image").getAttribute('src')
+document.getElementsByClassName(".product-image").onclick = () => {
+    document.getElementsByClassName('.pop-up').style.display = 'block'
+    document.getElementsByClassName('.product-image').src = document.querySelector("product-image").getAttribute('src')
 }
 
 
-document.querySelector('.pop-up-span').onclick = () => {
-    document.querySelector('.pop-up').style.display = 'none'
-}
+// document.querySelector('.pop-up-span').onclick = () => {
+//     document.querySelector('.pop-up').style.display = 'none'
+// }
+
+
+
+document.getElementsByClassName('.pop-up-span').addEventListener("click", function() {
+    // document.querySelector("pop-up").style.display = 'none';
+    // document.querySelector("span").style.display = "none";
+    document.getElementById("pop-up").style.display = "none"
+})
